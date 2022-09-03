5dbd9d5603193055b4677f30034c527fa3db7995
Shagulka <rob.falls.do@gmail.com>
2022-09-03 06:26:10 +0100

    new file:   assets/minesweeper_assets_manager.py

---
+++ assets/minesweeper_assets_manager.py
@@ -0,0 +1,34 @@
+import json
+
+flag = "🚩"
+mine = "💣"
+empty = "⬜"
+one = "1️⃣"
+two = "2️⃣"
+three = "3️⃣"
+four = "4️⃣"
+five = "5️⃣"
+six = "6️⃣"
+seven = "7️⃣"
+eight = "8️⃣"
+
+
+def load_assets(string):
+    with open(string) as json_assets:
+        assets_data = json.load(json_assets)
+        global flag, mine, empty, one, two, three, four, five, six, seven, eight
+        flag = assets_data["flag"]
+        mine = assets_data["mine"]
+        empty = assets_data["empty"]
+        one = assets_data["one"]
+        two = assets_data["two"]
+        three = assets_data["three"]
+        four = assets_data["four"]
+        five = assets_data["five"]
+        six = assets_data["six"]
+        seven = assets_data["seven"]
+        eight = assets_data["eight"]
+        return True
+
+
+
