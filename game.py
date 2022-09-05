1c335c119ced11f3e545387a7238df95f74c40d1
Shagulka <rob.falls.do@gmail.com>
2022-09-05 22:34:50 +0100

    it's working (kinda), will fix everything tomorrow. I'm just glad it's working at all. I'm not sure what I did to make it work, but I'm not complaining. I'll post the fix tomorrow.

--- game.py
+++ game.py
@@ -159,6 +159,154 @@ class PreGameMenu:
             return "menu"
 
 
+class Game:
+    def __init__(self, width, height, mines):
+        self.width = width
+        self.height = height
+        self.mines = mines
+        self.true_board = []
+        self.player_board = []
+        self.flagged = []
+        self.selected = [0, 0]
+        self.game_over = False
+        self.game_won = False
+        self.mine_locations = []
+        self.mine_count = 0
+        self.flag_count = 0
+        self.time = 0
+        self.generate_board()
+        self.generate_mines()
+        self.generate_numbers()
+        self.draw()
+    
+    def generate_board(self):
+        for i in range(self.height):
+            self.true_board.append([])
+            self.player_board.append([])
+            for j in range(self.width):
+                self.true_board[i].append(0)
+                self.player_board[i].append(0)
+    
+    def generate_mines(self):
+        for i in range(self.mines):
+            while True:
+                x = random.randint(0, self.width - 1)
+                y = random.randint(0, self.height - 1)
+                if self.true_board[y][x] != 9:
+                    self.true_board[y][x] = 9
+                    self.mine_locations.append([x, y])
+                    break
+    
+    def generate_numbers(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.true_board[i][j] != 9:
+                    self.true_board[i][j] = self.get_surrounding_mines(j, i)
+
+    def get_surrounding_mines(self, x, y):
+        count = 0
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if x + j >= 0 and x + j < self.width and y + i >= 0 and y + i < self.height:
+                    if self.true_board[y + i][x + j] == 9:
+                        count += 1
+        return count
+
+    def draw(self):
+        cls()
+        print(f"Time: {self.time}")
+        print(f"Flags: {self.flag_count}/{self.mines}")
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.player_board[i][j] == 0:
+                    if [j, i] == self.selected:
+                        print(assets.get_asset("selected"), end="")
+                    else:
+                        print(assets.get_asset("hidden"), end="")
+                elif self.player_board[i][j] == 1:
+                    if [j, i] == self.selected:
+                        print(assets.get_asset("selected"), end="")
+                    elif self.true_board[i][j] == 9:
+                        print(assets.get_asset("mine"), end="")
+                    elif self.true_board[i][j] == 0:
+                        print(assets.get_asset("empty"), end="")
+                    else:
+                        print(assets.get_asset(str(self.true_board[i][j])), end=" ")
+                elif self.player_board[i][j] == 2:
+                    print(assets.get_asset("flag"), end="")
+            print()
+        if self.game_over:
+            print("Game Over")
+        elif self.game_won:
+            print("You Win!")
+    
+    def move(self, direction):
+        if direction == "up":
+            if self.selected[1] > 0:
+                self.selected[1] -= 1
+        elif direction == "down":
+            if self.selected[1] < self.height - 1:
+                self.selected[1] += 1
+        elif direction == "left":
+            if self.selected[0] > 0:
+                self.selected[0] -= 1
+        elif direction == "right":
+            if self.selected[0] < self.width - 1:
+                self.selected[0] += 1
+
+    def flag(self):
+        x = self.selected[0]
+        y = self.selected[1]
+        if self.player_board[y][x] == 0:
+            self.player_board[y][x] = 2
+            self.flag_count += 1
+            self.flagged.append([x, y])
+        elif self.player_board[y][x] == 2:
+            self.player_board[y][x] = 0
+            self.flag_count -= 1
+            self.flagged.remove([x, y])
+    
+    def select(self):
+        x = self.selected[0]
+        y = self.selected[1]
+        if self.player_board[y][x] == 0:
+            if self.true_board[y][x] == 9:
+                self.player_board[y][x] = 1
+                self.game_over = True
+            elif self.true_board[y][x] == 0:
+                self.player_board[y][x] = 1
+                self.reveal_empty(x, y)
+            else:
+                self.player_board[y][x] = 1
+        if self.check_win():
+            self.game_won = True
+
+    
+    
+    def reveal_empty(self, x, y):
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if x + j >= 0 and x + j < self.width and y + i >= 0 and y + i < self.height:
+                    if self.player_board[y + i][x + j] == 0:
+                        self.player_board[y + i][x + j] = 1
+                        if self.true_board[y + i][x + j] == 0:
+                            self.reveal_empty(x + j, y + i)
+
+    def check_win(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                if self.player_board[i][j] == 0 and self.true_board[i][j] != 9:
+                    return
+        self.game_won = True
+    
+    def update(self):
+        if self.game_over or self.game_won:
+            return "menu"
+        self.time += 1
+        return "game"
+    
+    
+
 
     
 
@@ -181,6 +329,9 @@ while True:
         menu.draw()
     elif keyboard.read_key() == "enter":
         menu.select()
+        menu.draw()
+    elif keyboard.read_key() == "f":
+        menu.flag()
         menu.draw()
     elif keyboard.read_key() == "esc":
         break
--- minesweeper_assets_manager.py
+++ minesweeper_assets_manager.py
@@ -3,7 +3,7 @@ import json
 flag = "🚩"
 mine = "💣"
 empty = "⬜"
-nothing = "⬛"
+hidden = "⬛"
 selected = "🔳"
 one = "1️⃣"
 two = "2️⃣"
@@ -18,10 +18,13 @@ eight = "8️⃣"
 def load_assets(string):
     with open(string) as json_assets:
         assets_data = json.load(json_assets)
-        global flag, mine, empty, one, two, three, four, five, six, seven, eight
+        global flag, mine, empty, one, two, three, four, five, six, seven, eight, hidden, selected
         flag = assets_data["flag"]
         mine = assets_data["mine"]
         empty = assets_data["empty"]
+        hidden = assets_data["hidden"]
+        selected = assets_data["selected"]
+
         one = assets_data["one"]
         two = assets_data["two"]
         three = assets_data["three"]
@@ -31,6 +34,36 @@ def load_assets(string):
         seven = assets_data["seven"]
         eight = assets_data["eight"]
         return True
+
+def get_asset(string):
+    if string == "flag":
+        return flag
+    elif string == "mine":
+        return mine
+    elif string == "empty":
+        return empty
+    elif string == "hidden":
+        return hidden
+    elif string == "selected":
+        return selected
+    elif string == "1":
+        return one
+    elif string == "2":
+        return two
+    elif string == "3":
+        return three
+    elif string == "4":
+        return four
+    elif string == "5":
+        return five
+    elif string == "6":
+        return six
+    elif string == "7":
+        return seven
+    elif string == "8":
+        return eight
+    else:
+        return None
 
 
 
